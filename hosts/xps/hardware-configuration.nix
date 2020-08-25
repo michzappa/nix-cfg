0166f37b87ea887e023798fc045d71bfdab0f002
michzappa <zapprich@gmail.com>
2020-08-25 10:36:07 -0500

    disks by label

--- hosts/xps/hardware-configuration.nix
+++ hosts/xps/hardware-configuration.nix
@@ -14,12 +14,12 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ad6049b2-8bc2-4903-a1e8-0847ca14407b";
+    { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4D6B-1D1D";
+    { device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
 
